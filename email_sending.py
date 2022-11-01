dae18bdcc9d2b46d0846b2dfb596bb8890a71076
test999980 <114117010+test999980@users.noreply.github.com>
2022-11-01 15:37:29 +0900

    Update email_sending.py

--- email_sending.py
+++ email_sending.py
@@ -7,7 +7,7 @@ USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 TO_MAIL = os.environ.get("TO_MAIL")
 MAIL_TITLE = os.environ.get("MAIL_TITLE")
-MAIL_BODY =  = os.environ.get("MAIL_BODY")
+MAIL_BODY = os.environ.get("MAIL_BODY")
 
 
 message = f"""\
