450c86f35b3ba51ac3bd7708252b0dee0ce3b5ef
test999980 <114117010+test999980@users.noreply.github.com>
2022-11-01 15:25:40 +0900

    Update email_sending.py

--- email_sending.py
+++ email_sending.py
@@ -8,9 +8,8 @@ USER_PASSWORD = os.environ.get("USER_PASSWORD")
 TO_MAIL = os.environ.get("TO_MAIL")
 
 message = """\
-    Subject: Welcome Ubaydah
-
-    issue content
+    ${{ github.event.issue.title }}
+    ${{ github.event.issue.body }}
 """
 
 context = ssl.create_default_context()
