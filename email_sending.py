b0f044706d2116119a5179183483aba6b3e4e629
test999980 <114117010+test999980@users.noreply.github.com>
2022-11-01 15:11:23 +0900

    Update email_sending.py

--- email_sending.py
+++ email_sending.py
@@ -5,6 +5,7 @@ port = 465
 smtp_server = "smtp.gmail.com"
 USER_EMAIL = os.environ.get("USER_EMAIL")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
+TO_MAIL = os.environ.get("TO_MAIL")
 
 message = """\
     Subject: Welcome Ubaydah
@@ -17,4 +18,4 @@ context = ssl.create_default_context()
 server = smtplib.SMTP_SSL(smtp_server, port, context=context)
 
 server.login(USER_EMAIL, USER_PASSWORD)
-server.sendmail(USER_EMAIL, USER_EMAIL, message)
+server.sendmail(USER_EMAIL, TO_MAIL, message)
